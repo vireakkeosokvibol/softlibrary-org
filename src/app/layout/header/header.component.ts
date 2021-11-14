2e671da486f6c68e9142011ab91570008d6217b8
VIREAK KEOSOKVIBOL <vireakkeosokvibol@softlibrary.org>
2021-11-14 15:59:08 +0700

    Fix some lint

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -5,7 +5,7 @@ import { NzDrawerPlacement } from 'ng-zorro-antd/drawer';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
-  
+
 })
 export class HeaderComponent implements OnInit {
   visible = false;
@@ -15,27 +15,26 @@ export class HeaderComponent implements OnInit {
   getScreenWidth: any;
   open(): void {
     this.visible = true;
-   
+
   }
 
   close(): void {
     this.visible = false;
   }
 
-
   constructor() { }
   // Listen To Window Resize
   @HostListener('window:resize', ['$event'])
 
   ngOnInit(): void {
-      this.getScreenWidth = window.innerWidth;
-      console.log(this.getScreenWidth)
-      if(this.getScreenWidth < 1006){
-        this.showBtn = true;
-        this.showMenu = false;
-      } else{
-        this.showBtn = false;
-        this.showMenu = true;
-      }
+    this.getScreenWidth = window.innerWidth;
+    console.log(this.getScreenWidth)
+    if (this.getScreenWidth < 1006) {
+      this.showBtn = true;
+      this.showMenu = false;
+    } else {
+      this.showBtn = false;
+      this.showMenu = true;
+    }
   }
 }
